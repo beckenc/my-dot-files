9db5b1ac3718c20a6e39ae3479e8a0f693a69324
beckenc <claus.beckenbauer@diehl.com>
2020-03-14 19:50:14 +0100

    minor optimizations

--- bashrc.d/colors.sh
+++ bashrc.d/colors.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
 
-# +-------------------------------------------------
-# | Colors
-# +-------------------------------------------------
+# Color 'ls' output (default)
+LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.cfg=00;32:*.conf=00;32:*.diff=00;32:*.doc=00;32:*.ini=00;32:*.log=00;32:*.patch=00;32:*.pdf=00;32:*.ps=00;32:*.tex=00;32:*.txt=00;32:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:';
+export LS_COLORS
 
-export txtblk='\e[0;30m' # Black - Regular
+# dircolors --print-database uses its own built-in database
+# instead of using /etc/DIR_COLORS.  Try to use the external file
+# first to take advantage of user additions.
+# We run dircolors directly due to its changes in file syntax and
+# terminal name patching.
+export use_color=false
+if type -P dircolors >/dev/null ; then
+  # Enable colors for ls, etc.  Prefer ~/.dir_colors #64489
+  LS_COLORS=
+  if [[ -f ~/.dir_colors ]] ; then
+    eval "$(dircolors -b ~/.dir_colors)"
+  elif [[ -f /etc/DIR_COLORS ]] ; then
+    eval "$(dircolors -b /etc/DIR_COLORS)"
+  else
+    eval "$(dircolors -b)"
+  fi
+
+  # Note: We always evaluate the LS_COLORS setting even when it's the
+  # default.  If it isn't set, then `ls` will only colorize by default
+  # based on file attributes and ignore extensions (even the compiled
+  # in defaults of dircolors). #583814
+  if [[ -n ${LS_COLORS:+set} ]] ; then
+    use_color=true
+  else
+    # Delete it if it's empty as it's useless in that case.
+    unset LS_COLORS
+  fi
+else
+  # Some systems (e.g. BSD & embedded) don't typically come with
+  # dircolors so we need to hardcode some terminals in here.
+  case ${TERM} in
+    [aEkx]term*|rxvt*|gnome*|konsole*|screen|cons25|*color)
+      use_color=true
+      ;;
+    *)
+      # Delete it if it's empty as it's useless in that case.
+      unset LS_COLORS
+      ;;
+  esac
+fi
+
+if ! ${use_color}; then
+  # Terminal is not colorful. Be done now!
+  return
+fi
+
+export xtblk='\e[0;30m' # Black - Regular
 export txtred='\e[0;31m' # Red
 export txtgrn='\e[0;32m' # Green
 export txtylw='\e[0;33m' # Yellow
@@ -40,22 +86,3 @@ export txtrst='\e[0m'    # Text Reset
 
 # Color programs output
 export COLOR_OPTIONS='--color=auto'
-
-function print256Colors()
-{
-  for i in {0..255}; do
-      echo -e "\e[38;05;${i}m38;05;${i}m";
-  done | column -c 50 -s ' '
-
-  echo -en "\e[m"
-}
-
-function print8Colors()
-{
-  for i in {0..7}; do
-    echo -e "\e[0;3${i}m 0;3${i} - \e[1;3${i}m 1;3${i}";
-  done | column -c 4 -s ' '
-
-  echo -en "\e[m"
-}
-
