f12bbcb1a4229df7992ed53a72c5869ead07c9d2
cbe <github@claus-beckenbauer.de>
2021-09-29 07:10:56 +0200

    Update completions.sh
    removed obsolete completion

--- bashrc.d/completions.sh
+++ bashrc.d/completions.sh
@@ -43,47 +43,4 @@ _bar()
   COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
 }
 
-_ti2-ssh-service-tool()
-{
-  local cur prev opts
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
-  opts="--host --port -s --pass --ssh-key --ssh-port --ssh-status"
-
-  case "${prev}" in
-    --host)
-      COMPREPLY=( $(compgen -A hostname ${cur}) )
-      return 0
-      ;;
-    --port)
-      COMPREPLY=( 59595 59596 )
-      return 0
-      ;;
-    --pass)
-      COMPREPLY=( S38.vice )
-      return 0
-      ;;
-    --ssh-key)
-      COMPREPLY=( $(compgen -f ${cur}) )
-      return 0
-      ;;
-    --ssh-status)
-      COMPREPLY=( 0 1 )
-      return 0
-      ;;
-    --ssh-port)
-      COMPREPLY=( 22 )
-      return 0
-      ;;
-    *)
-      ;;
-  esac
-
-  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-}
-
 #complete -o bashdefault -F _bar bar
-complete -o bashdefault -F _ti2-ssh-service-tool ti2-ssh-service-tool
-
-#complete -A hostname hpr_login.exp
