720b20f80a9805019cb84e6d9b789bc9862a63b0
beckenc <claus.beckenbauer@diehl.com>
2021-09-29 07:22:48 +0200

    Fixed warning of KEY_BINDINGS is read only variable

--- bashrc.d/lib/fzf.sh
+++ bashrc.d/lib/fzf.sh
@@ -1,9 +1,9 @@
 # Wanna split fzf in a tmux pane
-declare -r KEY_BINDINGS='/usr/share/fzf/key-bindings.bash'
-declare -r COMPLETION='/usr/share/bash-completion/completions/fzf'
+declare -r FKEY_BINDINGS='/usr/share/fzf/key-bindings.bash'
+declare -r FCOMPLETION='/usr/share/bash-completion/completions/fzf'
 
-test -f $KEY_BINDINGS && source $KEY_BINDINGS
-test -f $COMPLETION && source $COMPLETION
+test -f $FKEY_BINDINGS && source $FKEY_BINDINGS
+test -f $FCOMPLETION && source $FCOMPLETION
 
 export FZF_TMUX=1
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
