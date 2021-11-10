a1f11c42009fcbf6ab18a6b18663de271b542cc9
beckenc <claus.beckenbauer@diehl.com>
2021-11-10 14:47:51 +0100

    unset not longer needed variables

--- bashrc.d/lib/fzf.sh
+++ bashrc.d/lib/fzf.sh
@@ -1,11 +1,14 @@
 # Wanna split fzf in a tmux pane
-declare -r FKEY_BINDINGS='/usr/share/fzf/key-bindings.bash'
-declare -r FCOMPLETION='/usr/share/bash-completion/completions/fzf'
+declare FKEY_BINDINGS='/usr/share/fzf/key-bindings.bash'
+declare FCOMPLETION='/usr/share/bash-completion/completions/fzf'
 
 test -f $FKEY_BINDINGS && source $FKEY_BINDINGS
 test -f $FCOMPLETION && source $FCOMPLETION
 
 export FZF_TMUX=1
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+
+unset FKEY_BINDINGS
+unset FCOMPLETION
 
 #EOF
